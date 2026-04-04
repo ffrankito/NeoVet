34be28622a17455a59cd0029a16d6bbf8c360eed
ffrankito <zfi1811@gmail.com>
2026-04-03 21:05:34 -0300

    .

--- crm/src/db/index.ts
+++ crm/src/db/index.ts
@@ -4,17 +4,9 @@ import * as schema from "./schema";
 
 const connectionString = process.env.DATABASE_URL!;
 
-if (connectionString) {
-  const parsed = new URL(connectionString);
-  console.log("DB protocol:", parsed.protocol);
-  console.log("DB username:", parsed.username);
-  console.log("DB host:", parsed.host);
-  console.log("DB password length:", parsed.password.length);
-}
-
 const globalForDb = globalThis as unknown as { db: ReturnType<typeof drizzle> };
 
-const client = postgres(connectionString, { ssl: "require", max: 1 });
+const client = postgres(connectionString, { max: 1 });
 
 export const db = globalForDb.db ?? drizzle(client, { schema });
 
